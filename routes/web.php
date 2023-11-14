2bfe31c7956f240e2c345e1922d424666f646bb5
developermkarim <m.karimcu@gmail.com>
2023-11-15 00:53:47 +0600

    admin template segment and frontend home page

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
+use App\Http\Controllers\ProfileController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,9 +16,8 @@ use App\Http\Controllers\AgentController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// User Frontend All Route
+Route::get('/', [UserController::class, 'Index']);
 
 
 Route::get('/dashboard', function () {
